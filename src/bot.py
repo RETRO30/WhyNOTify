2a4042b1490269b0d6a5b57730ae82b7218d24c5
RETRO30 <ssh-off@ya.ru>
2025-01-14 00:05:24 +0500

    Сделал основу

---
+++ src/bot.py
@@ -0,0 +1,6 @@
+class Bot:
+    def __init__(self):
+        pass
+    
+    def send_messages_to_channels(self):
+        pass
---
+++ src/manager.py
@@ -0,0 +1,13 @@
+class Task:
+    def __init__(self):
+        pass
+    
+    def run(self):
+        pass
+
+class TaskManager:
+    def __init__(self):
+        pass
+    
+    def start(self):
+        pass
