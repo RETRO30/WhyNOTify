e019ed2f4ddf060f13c4dd2b099172d1ce2398aa
RETRO30 <ssh-off@ya.ru>
2025-01-21 16:57:13 +0200

    убрал аунтификацию

--- notifications/stickers.py
+++ notifications/stickers.py
@@ -167,15 +167,15 @@ class Stickers:
             logger.error(f"{self.account} | Auth data is invalid")
             return Response(Status.ERROR, Description.SESSION_EXPIRED)
 
-        auth_data: AuthData = response.data
-        try:
-            response: Response = await self.api.auth(auth_data=auth_data)
-            if response.status == Status.ERROR:
-                return response
-        except Exception as e:
-            logger.error(f"{self.account} | Error setting up stickers: {e}")
-            await send_tech_messages(f"{self.account} | Error setting up stickers: {e}")
-            return Response(Status.ERROR, Description.INVALID_RESPONSE, data=str(e))
+        # auth_data: AuthData = response.data
+        # try:
+        #     response: Response = await self.api.auth(auth_data=auth_data)
+        #     if response.status == Status.ERROR:
+        #         return response
+        # except Exception as e:
+        #     logger.error(f"{self.account} | Error setting up stickers: {e}")
+        #     await send_tech_messages(f"{self.account} | Error setting up stickers: {e}")
+        #     return Response(Status.ERROR, Description.INVALID_RESPONSE, data=str(e))
         return Response(Status.SUCCESS, Description.OK)
 
     async def check_updates(self, last_collections: Collection | None) -> Response:
